7ddf87f86f152c3507bc7e20bd145880c62275cd
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-03-31 13:51:41 +0600

    logo and notFound component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from "react";
+import logo from './logo.png'
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,26 +10,31 @@ import {
 import Home from './components/Home/Home';
 import Blood from './components/Blood/Blood';
 import Missing from './components/Missing/Missing';
+import NotFound from './components/NotFound/NotFound';
 function App() {
   return (
     <div className="App">
       <Router>
-      <div className='header'>
-            <div className="title-section">
-                <h1>Shondhan</h1>
+        <div className='header'>
+          <div className="title-section">
+            <div className="group">
+              <img src={logo} alt="" />
+              <h1>Shondhan</h1>
             </div>
 
-            <nav>
-                <Link to="/home">Home</Link>
-                <Link to="/blood_donors">Blood Donors</Link>
-                <Link to="/missing_post">Missing Post</Link>
-            </nav>
+          </div>
+
+          <nav>
+            <Link to="/home">Home</Link>
+            <Link to="/blood_donors">Blood Donors</Link>
+            <Link to="/missing_post">Missing Post</Link>
+          </nav>
         </div>
         <Switch>
           <Route exact path='/'>
             <Home></Home>
           </Route>
-          <Route path='/home'>
+          <Route exact path='/home'>
             <Home></Home>
           </Route>
           <Route exact path='/blood_donors'>
@@ -38,7 +44,7 @@ function App() {
             <Missing></Missing>
           </Route>
           <Route path='*'>
-
+            <NotFound></NotFound>
           </Route>
         </Switch>
       </Router>
